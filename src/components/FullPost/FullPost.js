4544fa72bdcec0214eaee9f448caeda4ccafaab5
Ben <bengedi@gmail.com>
2018-09-24 18:12:15 +0300

    sending a delete request

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -16,6 +16,14 @@ class FullPost extends Component {
                 });
         }
     }
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                console.log(response);
+                this.setState({loadedPost: null});
+            });
+    }
     render () {
         let post = <p style={{ textAlign: 'center' }}>
         {this.props.id ? 'Loading...' : 'Please select a Post!'}
@@ -26,7 +34,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
                     </div>
                 </div>
             );
