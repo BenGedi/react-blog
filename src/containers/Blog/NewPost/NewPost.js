54e050ef8037849778e6132f9837e3ca9e17dc40
Ben <bengedi@gmail.com>
2018-09-27 01:42:16 +0300

    conditinal route redirect

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 import './NewPost.css';
 
@@ -7,7 +8,8 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Ben'
+        author: 'Ben',
+        submitted: false
     }
 
     postDataHandler = () => {
@@ -17,12 +19,18 @@ class NewPost extends Component {
             author: this.state.author
         };
         axios.post('/posts', post)
-            .then(response => console.log(response));
+            .then(response => {
+                console.log(response);
+                this.setState({ submitted: true });
+            });
     }
 
     render () {
         return (
             <div className="NewPost">
+                {   this.state.submitted &&
+                    <Redirect to="/posts"/>
+                }
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
