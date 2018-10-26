f69650ba31c1e31fa5bc59e55a9703605120c35b
Ben <bengedi@gmail.com>
2018-10-26 06:26:40 +0300

    dynamic import using React.lazy()

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,11 @@
 import './Blog.css'
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import Posts from '../../containers/Blog/Posts/Posts';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 // import NewPost from './NewPost/NewPost';
 import asyncComponent from '../../hoc/asyncComponent';
+// import TestPage from './testPage/testPage';
+const TestPage = React.lazy(() => import('./testPage/testPage'));
 const AsyncNewPost = asyncComponent(() => {
     // dynamic import approach
     return import('./NewPost/NewPost');
@@ -20,19 +22,28 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink
-                                to="/posts/"
-                                exact
-                                activeClassName="my-active"
-                                activeStyle={{
-                                    color: '#fa923f',
-                                    textDecoration: 'underline'
-                                }}>Posts</NavLink></li>
-                            <li><NavLink to={{
-                                pathname: '/new-post',
-                                hash: '#submit',
-                                search: '?quick-submit=true'
-                            }}>New Post</NavLink></li>
+                            <li>
+                                <NavLink
+                                    to="/posts/"
+                                    exact
+                                    activeClassName="my-active"
+                                    activeStyle={{
+                                        color: '#fa923f',
+                                        textDecoration: 'underline'
+                                    }}>Posts</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={{
+                                    pathname: '/new-post',
+                                    hash: '#submit',
+                                    search: '?quick-submit=true'
+                                }}>New Post</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={{
+                                    pathname: '/test-page',
+                                }}>Test Page</NavLink>
+                            </li>
                         </ul>
                     </nav>
                 </header>
@@ -40,6 +51,11 @@ class Blog extends Component {
                 <Switch>
                     {this.state.auth && <Route path="/new-post" component={AsyncNewPost} />}
                     <Route path="/posts" component={Posts} />
+                    <Route path="/test-page" render={() => (
+                        <Suspense fallback={<div>Loading...</div>}>
+                            <TestPage />
+                        </Suspense>)
+                    } />
                     <Redirect from="/" to="/posts" />
                     {/*<Route path="/" exact component={Posts} />*/}
                 </Switch>
---
+++ src/containers/Blog/testPage/testPage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const testPage = () => <h1>Some Test Page</h1>;
+
+export default testPage;
